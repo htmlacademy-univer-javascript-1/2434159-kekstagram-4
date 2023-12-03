72691eda8f5dcfad247189224944fb8a125dfd9f
Andrei <tcheglakov.andrey@yandex.ru>
2023-12-03 16:13:46 +0500

    5.2

--- js/functions.js
+++ js/functions.js
@@ -9,3 +9,13 @@ const isPalindromeStr = (str) => {
   }
   return true;
 };
+
+function checkWorkTime(dayBegin, dayEnd, meetingBegin, duration) {
+  const beginTime = dayBegin.split(':');
+  const endTime = dayEnd.split(':');
+  const meetingBeginTime = meetingBegin.split(':');
+  const dayBeginMinutes = (+beginTime[0]) * 60 + (+beginTime[1]);
+  const dayEndMinutes = (+endTime[0]) * 60 + (+endTime[1]);
+  const meetingBeginMinutes = (+meetingBeginTime[0]) * 60 + (+meetingBeginTime[1]);
+  return dayBeginMinutes <= meetingBeginMinutes && meetingBeginMinutes + duration <= dayEndMinutes;
+}
