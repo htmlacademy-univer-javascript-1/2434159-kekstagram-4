df092f1f39657203b55622860590f03924a564d2
Andrei <tcheglakov.andrey@yandex.ru>
2023-12-12 20:19:49 +0500

    8.15

--- js/big-photo.js
+++ js/big-photo.js
@@ -1,6 +1,35 @@
+import { COMMENT_STEP } from './data.js';
+
 const commentsTemplate = document.querySelector('.social__comment');
-document.querySelector('.social__comment-count').classList.add('hidden');
-document.querySelector('.comments-loader').classList.add('hidden');
+const bodyElement = document.body;
+const commentsLoader = bodyElement.querySelector('.comments-loader');
+const activeComments = document.querySelector('.loaded_comments');
+const bigPhoto = document.querySelector('.big-picture');
+const closeViewPopupBtn = document.querySelector('.big-picture__cancel');
+const loadCommentsBtn = bigPhoto.querySelector('.social__comments-loader');
+
+const renderMoreComments = () => {
+  let currentComment = document.querySelector('.social__comment.hidden');
+  let i = 0;
+  for (let j = i; i < j + COMMENT_STEP; i++) {
+    if (!currentComment) {
+      commentsLoader.classList.add('hidden');
+      break;
+    }
+    currentComment.classList.remove('hidden');
+    currentComment = currentComment.nextElementSibling;
+    if (!currentComment) {
+      commentsLoader.classList.add('hidden');
+      activeComments.textContent = +activeComments.textContent + 1;
+      break;
+    }
+  }
+  activeComments.textContent = +activeComments.textContent + i;
+};
+
+const onCloseBtnClick = () => closePhoto();
+
+const onLoadCommentsBtn = () => renderMoreComments();
 
 const loadComments = (commentsContainer, comments) => {
   const commentsFragment = document.createDocumentFragment();
@@ -9,6 +38,7 @@ const loadComments = (commentsContainer, comments) => {
     comment.querySelector('.social__picture').src = comments[i].avatar;
     comment.querySelector('.social__picture').alt = comments[i].name;
     comment.querySelector('.social__text').textContent = comments[i].message;
+    comment.classList.add('hidden');
     commentsFragment.append(comment);
   }
   commentsContainer.innerHTML = '';
@@ -22,25 +52,31 @@ const onDocumentKeydown = (evt) => {
 };
 
 function openPhoto(url, description, likes, comments) {
-  const openedPhoto = document.querySelector('.big-picture');
-  openedPhoto.classList.remove('hidden');
+  bigPhoto.classList.remove('hidden');
   document.body.classList.add('modal-open');
-  openedPhoto.querySelector('.big-picture__img img').src = url;
-  openedPhoto.querySelector('.likes-count').textContent = likes;
-  openedPhoto.querySelector('.comments-count').textContent = comments.length;
-  openedPhoto.querySelector('.social__caption').textContent = description;
-  openedPhoto.querySelector('.loaded_comments').textContent = comments.length;
+  bigPhoto.querySelector('.big-picture__img img').src = url;
+  bigPhoto.querySelector('.likes-count').textContent = likes;
+  bigPhoto.querySelector('.comments-count').textContent = comments.length;
+  bigPhoto.querySelector('.social__caption').textContent = description;
+  activeComments.textContent = '0';
   const commentsContainer = document.querySelector('.social__comments');
+
   loadComments(commentsContainer, comments);
+
+  commentsLoader.classList.remove('hidden');
+  renderMoreComments();
+
   document.addEventListener('keydown', onDocumentKeydown);
-  openedPhoto.querySelector('.big-picture__cancel').addEventListener('click', closePhoto);
+  bigPhoto.querySelector('.big-picture__cancel').addEventListener('click', closePhoto);
+  loadCommentsBtn.addEventListener('click', onLoadCommentsBtn);
+  closeViewPopupBtn.addEventListener('click', onCloseBtnClick);
 }
 
-function closePhoto(evt) {
-  document.body.classList.remove('modal-open');
-  document.querySelector('.big-picture').classList.add('hidden');
-  evt.target.removeEventListener('click', closePhoto);
+function closePhoto() {
+  bodyElement.classList.remove('modal-open');
   document.removeEventListener('keydown', onDocumentKeydown);
+  loadCommentsBtn.removeEventListener('click', onLoadCommentsBtn);
+  bigPhoto.classList.add('hidden');
 }
 
 export { openPhoto };
