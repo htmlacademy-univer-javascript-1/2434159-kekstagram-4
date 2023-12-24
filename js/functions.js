f9465473e36fc54027244fdeb0aa683d5b69bf53
Andrei <tcheglakov.andrey@yandex.ru>
2023-12-24 23:24:10 +0500

    11.1

--- js/functions.js
+++ js/functions.js
@@ -1,27 +1,64 @@
-const isValidStrokeLength = (str, maxLength) => str.length <= maxLength;
-
-isValidStrokeLength('asdasdasd', 8);
-
-const isPalindromeStr = (str) => {
-  const len = str.length;
-  for (let i = 0; i < len / 2; i++) {
-    if (str.at(i) !== str.at(len - 1 - i)) {
-      return false;
-    }
-  }
-  return true;
-};
-
-isPalindromeStr('asdsa');
-
-function checkWorkTime(dayBegin, dayEnd, meetingBegin, duration) {
-  const beginTime = dayBegin.split(':');
-  const endTime = dayEnd.split(':');
-  const meetingBeginTime = meetingBegin.split(':');
-  const dayBeginMinutes = (+beginTime[0]) * 60 + (+beginTime[1]);
-  const dayEndMinutes = (+endTime[0]) * 60 + (+endTime[1]);
-  const meetingBeginMinutes = (+meetingBeginTime[0]) * 60 + (+meetingBeginTime[1]);
-  return dayBeginMinutes <= meetingBeginMinutes && meetingBeginMinutes + duration <= dayEndMinutes;
+//первое задание
+function checkLength (line, maxLength) {
+  return line.length <= maxLength;
 }
 
-checkWorkTime('14:00', '21:00', '15:00', '400');
+checkLength('проверяемая строка', 20);
+checkLength('проверяемая строка', 18);
+checkLength('проверяемая строка', 10);
+
+//второе задание
+function checkPalindrome(line) {
+  line = line.toLowerCase();
+  line = line.replaceAll(' ', '');
+  for (let letter = 0; letter <= line.length/2; letter++) {
+    if (!(line[letter] === line[line.length - letter - 1])) {
+      return false;
+    }
+    return true;
+  }
+}
+
+checkPalindrome('топот');
+checkPalindrome('ДовОд');
+checkPalindrome('Кекс');
+checkPalindrome('Лёша на полке клопа нашёл');
+
+//третье задание
+function returnNumber(line) {
+  line = String(line);
+  let finalNumber = '';
+  line = line.replaceAll(' ', '');
+  for (let i = 0; i < line.length; i++){
+    if (!isNaN(line[i])) {
+      finalNumber += line[i];
+    }
+  }
+  return Number(finalNumber);
+}
+returnNumber('2023 год');
+returnNumber('ECMAScript 2022');
+returnNumber('1 кефир, 0.5 батона');
+returnNumber('агент 007');
+returnNumber('а я томат');
+returnNumber(2023);
+returnNumber(-1);
+returnNumber(1.5);
+
+//доп задание
+function TimeVisitAnswer(dayStart, dayEnd, visitStart, visitLength) {
+  dayEnd = dayEnd.split(':');
+  dayStart = dayStart.split(':');
+  visitStart = visitStart.split(':');
+  const probably = visitStart[0]*60 + +visitStart[1] + +visitLength;
+  if (dayStart[0]*60 + +dayStart[1] > visitStart[0]*60 + +visitStart[1]) {
+    return false;
+  }
+  return probably <= (dayEnd[0]*60 + +dayEnd[1]);
+}
+
+TimeVisitAnswer('08:00', '17:30', '14:00', 90);
+TimeVisitAnswer('8:0', '10:0', '8:0', 120);
+TimeVisitAnswer('08:00', '14:30', '14:00', 90);
+TimeVisitAnswer('14:00', '17:30', '08:0', 90);
+TimeVisitAnswer('8:00', '17:30', '08:00', 900);
--- js/main.js
+++ js/main.js
@@ -1,4 +1,14 @@
-import { getPhotosCollection } from './utils.js';
-import { drawPictures } from './mini-photos.js';
+import './formLogic.js';
+import './photoFilters.js';
+import { getData } from './api.js';
+import { drawSmall } from './draw.js';
+import { errorServerMessage, closeError } from './util.js';
 
-drawPictures(getPhotosCollection());
+getData()
+  .then((data) => {
+    drawSmall(data);
+  })
+  .catch(() => {
+    errorServerMessage('Не получили ответ от сервера');
+    setTimeout(() => {closeError();}, 2000);
+  });
